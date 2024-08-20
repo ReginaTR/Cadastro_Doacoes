8277dbb243b84ab9dd5b4d9f1e3b260f47b2f44a
ReginaTR <regininhar@hotmail.com>
2024-08-20 14:40:07 -0300

    feat: validates on Institutions

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,2 +1,7 @@
 class Institution < ApplicationRecord
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence:true
+  validates :cnpj, presence:true
+  validates :phone, presence:true
+  validates :address, presence:true
 end
