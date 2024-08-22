53e934e3a79d771fba6f036d6771906b40404c62
ReginaTR <regininhar@hotmail.com>
2024-08-22 17:16:29 -0300

    feat: rename sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,21 +13,22 @@ class ApplicationController < ActionController::Base
     !!current_institution
   end
 
-  def sign_in(institution)
+  def custom_sign_in(institution)
     session[:institution_id] = institution.id
   end
 
-  def sign_out
+  def custom_sign_out
     session[:institution_id] = nil
   end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :name])
   end
+
   def after_sign_in_path_for(resource)
     institutions_path
   end
-
 end
