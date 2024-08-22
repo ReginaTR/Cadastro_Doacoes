0ccbb6941dd9dcd79ae6306dacb0e93169a22958
ReginaTR <regininhar@hotmail.com>
2024-08-22 14:04:45 -0300

    Ignore

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem "pg", "~> 1.1"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
+gem 'rspec-rails', '~> 6.0'
+gem 'shoulda-matchers', '~> 5.0'
+
 # Gemfile
 gem "railsui", github: "getrailsui/railsui"
 
@@ -59,6 +62,8 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
 
+  gem 'rspec-rails', '~> 6.0'
+
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
@@ -70,4 +75,6 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_29_185658) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_20_180952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_29_185658) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "institutions", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "cnpj"
+    t.string "address"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_institutions_on_email", unique: true
   end
 
   create_table "users", force: :cascade do |t|
