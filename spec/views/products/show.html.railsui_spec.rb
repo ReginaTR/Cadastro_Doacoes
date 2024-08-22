439f1081d29f0558731dbb1c288644299326dc76
ReginaTR <regininhar@hotmail.com>
2024-08-22 18:24:06 -0300

    feat: create Products

---
+++ spec/views/products/show.html.railsui_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "products/show", type: :view do
+  before(:each) do
+    assign(:product, Product.create!(
+      name: "Name",
+      description: "MyText",
+      quantity: 2,
+      status: "Status",
+      institution: nil
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(/Status/)
+    expect(rendered).to match(//)
+  end
+end
